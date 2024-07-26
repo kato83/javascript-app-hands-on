4c408ddbfb4662aa7001dcfbeff719ede40401d2
kato83 <katomeganeb83@gmail.com>
2024-07-27 01:25:42 +0900

    init commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# JavaScriptアプリケーションハンズオン
+
+Jamstack構成なWEBアプリケーションを開発してみましょう。
+
+> [!NOTE]
+> **Jamstack**とはWebアプリケーション開発におけるシステム構成の一種で、`JavaScript` `API` `Markup` の3つの技術を用いたシステム構成を意味する。
+
+# セットアップ
+
+[セットアップ · kato83/javascript-app-hands-on-2024 Wiki](https://github.com/kato83/javascript-app-hands-on-2024/wiki/%E3%82%BB%E3%83%83%E3%83%88%E3%82%A2%E3%83%83%E3%83%97) を参考にセットアップを実施してください。
+
+# 設計
+
---
+++ backend/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const session = require('express-session');
+const RedisStore = require('connect-redis').default;
+const Redis = require('ioredis');
+const { PrismaClient, Prisma } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+const redisClient = new Redis(process.env.REDIS_URL);
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+  })
+);
+
+app.get('/todo', async (req, res) => {
+  const prisma = new PrismaClient();
+  await prisma.todo.create({
+    data: {
+      title: `TEST: ${new Date().toISOString()}`,
+      completed: false,
+      createdAt: new Date(),
+    }
+  })
+  const result = await prisma.todo.findMany({});
+  res.json({ message: 'HELLO WORLD!', todos: result });
+});
+
+app.post('/todo', async (req, res) => {
+  const { title } = req.body;
+  const todo = await prisma.todo.create({ data: { title } });
+  res.json(todo);
+});
+
+app.get('/todos', async (req, res) => {
+  const todos = await prisma.todo.findMany();
+  res.json(todos);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
